6d4914fe03979df1a40064482bd51085448d2757
TsukemonoGit <102149418+TsukemonoGit@users.noreply.github.com>
2024-04-13 13:14:18 +0900

    fix header footer

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ export function Header() {
   return (
     <header>
       <Container maxWidth="lg" sx={{ mt: 2, mb: 2 }}>
-        <Typography variant="h4">Bookmark Data Recovery Tool</Typography>
+        <Typography variant="h4">Nostr Bookmark Recovery Tool</Typography>
         {/* <Typography variant="body2">Search from Various Relays and Retrieve and Save Nostr Bookmarks. </Typography> */}
       </Container>
     </header>
