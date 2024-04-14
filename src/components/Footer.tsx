d10f01a64864657f59647771257e59d61641e6a7
TsukemonoGit <102149418+TsukemonoGit@users.noreply.github.com>
2024-04-14 15:56:54 +0900

    test

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,13 +5,21 @@ import {
   Card,
   Container,
   Divider,
+  IconButton,
   Link,
   List,
   ListItem,
+  Stack,
   Theme,
   alpha,
   useTheme,
 } from "@suid/material";
+import {
+  githubIcon,
+  lightningIcon,
+  lightningIcon2,
+  nostrIcon,
+} from "../libs/icons";
 
 export function Footer() {
   const theme = useTheme();
@@ -30,40 +38,81 @@ export function Footer() {
           sx={{
             display: "flex",
             flexDirection: "row",
-            // alignItems: "center",
-            "& > *": {
-              m: 1,
-            },
+            alignItems: "center",
+            justifyContent: "center",
+            gap: "2px",
           }}
         >
-          <Link
-            href={"https://github.com/TsukemonoGit/nos-bookmark-areyatu"}
-            target="_blank"
-            rel="noopener noreferrer"
-            color="primary"
-            underline="none"
-            sx={{
-              py: 2,
-              px: 1,
-              fontWeight: "bold",
+          <IconButton
+            aria-label="ソースコードへのリンク"
+            onClick={() => {
+              window.open(
+                "https://github.com/TsukemonoGit/nos-bookmark-areyatu",
+                "_blank",
+                "noreferrer"
+              );
             }}
+            sx={{}}
           >
-            SOURCE
-          </Link>
-          <Link
-            href={"https://github.com/nostr-protocol/nips/blob/master/51.md"}
-            target="_blank"
-            rel="noopener noreferrer"
-            color="primary"
-            underline="none"
+            <Stack
+              innerHTML={githubIcon}
+              sx={{
+                fill: useTheme().palette.text.primary,
+                width: "24px",
+              }}
+            />
+          </IconButton>
+          <IconButton
+            aria-label="nostterのmonoページへのリンク"
+            onClick={() => {
+              window.open(
+                "https://nostter.app/mono@tsukemonogit.github.io",
+                "_blank",
+                "noreferrer"
+              );
+            }}
+            sx={{}}
+          >
+            <Stack
+              innerHTML={nostrIcon}
+              sx={{
+                width: "28px",
+              }}
+            />
+          </IconButton>
+          <IconButton
+            id="nostr-zap-target"
+            aria-label="Zapボタン"
+            data-npub="npub1sjcvg64knxkrt6ev52rywzu9uzqakgy8ehhk8yezxmpewsthst6sw3jqcw"
+            data-relays="wss://nostr.mutinywallet.com,wss://nos.lol,wss://nostr.wine,wss://relay.nostr.band"
+            data-note-id="note160q4w6ar27qjpwrfxhnwv5ra3hpj552m230kau4vg5sf3d9das3q0hvglm"
+          >
+            <Stack
+              innerHTML={lightningIcon2}
+              sx={{
+                fill: useTheme().palette.warning.light,
+              }}
+            />
+          </IconButton>
+          <IconButton
+            aria-label="NIP-51へのリンク"
+            onClick={() => {
+              window.open(
+                "https://github.com/nostr-protocol/nips/blob/master/51.md",
+                "_blank",
+                "noreferrer"
+              );
+            }}
             sx={{
+              borderRadius: "10px",
+              fontSize: "medium",
               py: 2,
               px: 1,
               fontWeight: "bold",
             }}
           >
             NIP-51
-          </Link>
+          </IconButton>
         </Box>
       </Container>
     </footer>
